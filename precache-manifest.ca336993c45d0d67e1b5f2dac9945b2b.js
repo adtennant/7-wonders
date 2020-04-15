43c8046490426f04948ad84224acded4ff11c8a9
adtennant <adtennant@users.noreply.github.com>
2020-04-15 16:48:41 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.ca336993c45d0d67e1b5f2dac9945b2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbfac31024d64b7caa947191eddaa31",
+    "url": "/7-wonders/index.html"
+  },
+  {
+    "revision": "799696455f2878903158",
+    "url": "/7-wonders/static/css/main.a67a5ba4.chunk.css"
+  },
+  {
+    "revision": "c4b63f4343e2438c3e26",
+    "url": "/7-wonders/static/js/2.f0c27f61.chunk.js"
+  },
+  {
+    "revision": "799696455f2878903158",
+    "url": "/7-wonders/static/js/main.39fc0dc7.chunk.js"
+  },
+  {
+    "revision": "8abacfd2d8c204f851a4",
+    "url": "/7-wonders/static/js/runtime-main.a8a55460.js"
+  },
+  {
+    "revision": "c7cd32e6f045ec76896e27c09d82ece4",
+    "url": "/7-wonders/static/media/coin.c7cd32e6.png"
+  },
+  {
+    "revision": "3f50e8c9f46882008f20f7208308e7cb",
+    "url": "/7-wonders/static/media/compass.3f50e8c9.png"
+  },
+  {
+    "revision": "4279314731b4e4d1b288c51d2d148265",
+    "url": "/7-wonders/static/media/gear.42793147.png"
+  },
+  {
+    "revision": "6f2cc17635ee0ec85f3c1147a5ee9232",
+    "url": "/7-wonders/static/media/tablet.6f2cc176.png"
+  }
+]);
